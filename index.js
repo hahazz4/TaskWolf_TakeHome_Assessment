2580fea549e19d1b0eab9fc1a58a75de3df8966f
Zeerak Asim <Z_zee09@hotmail.com>
2024-11-24 03:59:04 -0500

    Retrieved the first 30 titles, and dates of the newest articles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
+const { title } = require("process");
 
 async function sortHackerNewsArticles() {
   // launch browser
@@ -9,11 +10,55 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+  await page.waitForTimeout(5000);
+  
+  // retrieve the class to get access to the date of each article
+  // const articleDate = await page.$$eval('.subline', articles => {
+  //   const data = []
+  //   articles.forEach(article => {
+  //     const aDate = article.querySelector('.age')
+  //     const date = aDate ? aDate.title: null;
+  //     data.push({date})
+  //   })
+  //   return data
+  // })
+  
+  // const articleTitle = await page.$$eval('.title', articles => {
+  //   const data = []
+  //   articles.forEach(article => {
+  //     const aTitle = article.querySelector('.titleline > a')
+  //     const title = title ? aTitle.title: null;
+  //     data.push({title})
+  //   })
+  //   return data
+  // })
+    
+  // dates = articleDate
+  // titles = articleTitle
+  // console.log(dates)
+  // console.log(titles)
+  // const articleAges = page.waitForSelector('.age')
 
-  // retrieve the id for the date of each article
-  const articleDate = document.getElementsByClassName("age");
-  print(articleDate)
+  // getting all the article dates
+  const articleAges = await page.$$eval('.subline > .age', ages => {
+    return ages.map(age => age.getAttribute('title'))
+  })
+  
+  // getting all the article titles
+  const articleTitles = await page.$$eval('.titleline > a', anchors => {
+    return anchors.map(anchor => ({
+      title: anchor.innerText,
+      url: anchor.href
+    }))
+  })
 
+  // const articleIDs = await page.$$eval('.subline > .age > a', link)
+
+  // combining each article's date, id, and title
+
+  console.log(articleAges)
+  console.log(articleTitles)
+  await browser.close();  
 }
 
 (async () => {
